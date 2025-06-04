60c5af6301c0afac4117654e429117ba419f5d68
Fahim Reza <fahim.bdctg71@gmail.com>
2025-06-04 23:00:29 +0600

    added DbContext and ApplicationUser DbSet

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AccMgt.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccMgt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
