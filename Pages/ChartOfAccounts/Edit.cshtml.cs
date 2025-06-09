d5d949d51bf41f66a56c8f5c265ff390d5be1d2a
Fahim Reza <fahim.bdctg71@gmail.com>
2025-06-09 21:57:25 +0600

    Updated role base access permission

--- Pages/ChartOfAccounts/Edit.cshtml.cs
+++ Pages/ChartOfAccounts/Edit.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,6 +9,7 @@ using System.Data;
 
 namespace AccMgt.Pages.ChartOfAccounts
 {
+    [Authorize(Roles = "Admin,Accountant")]
     public class EditModel : PageModel
     {
         private readonly IConfiguration _configuration;
